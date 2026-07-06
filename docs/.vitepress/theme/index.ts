29427734a330d92abb74b598cdc61c9f02c92971
baiyutang <irichardwang@gmail.com>
2026-07-07 00:30:21 +0800

    feat: add Vercel Analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import { inject } from '@vercel/analytics'
 import VersionFooter from './VersionFooter.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  enhanceApp() {
+    inject()
+  },
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'layout-bottom': () => h(VersionFooter),
